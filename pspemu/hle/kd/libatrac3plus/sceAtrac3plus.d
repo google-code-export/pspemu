18ef036dccfc567901b00bce8fefed23a780f5b6
soywiz <soywiz@cb060aac-a07e-11de-89ee-f3da9a46d971>
2011-05-23 08:23:09 +0000

    - More dummy sceAudio and sceAtrac3plus

--- pspemu/hle/kd/libatrac3plus/sceAtrac3plus.d
+++ pspemu/hle/kd/libatrac3plus/sceAtrac3plus.d
@@ -13,6 +13,9 @@ class sceAtrac3plus : ModuleNative {
 	void initNids() {
 		mixin(registerd!(0x7A20E7AF, sceAtracSetDataAndGetID));
 		mixin(registerd!(0x868120B5, sceAtracSetLoopNum));
+		mixin(registerd!(0x9AE849A7, sceAtracGetRemainFrame));
+		mixin(registerd!(0x6A8C3CD5, sceAtracDecodeData));
+		mixin(registerd!(0x61EB33F5, sceAtracReleaseAtracID));
 	}
 	
 	/**
@@ -24,6 +27,7 @@ class sceAtrac3plus : ModuleNative {
 	 * @return the new atrac ID, or < 0 on error 
 	*/
 	int sceAtracSetDataAndGetID(void *buf, SceSize bufsize) {
+		logWarning("Not implemented sceAtracSetDataAndGetID");
 		Atrac3Object atrac3Object = new Atrac3Object();
 		atrac3Object.buf = buf;
 		atrac3Object.bufsize = bufsize;
@@ -42,6 +46,59 @@ class sceAtrac3plus : ModuleNative {
 	int sceAtracSetLoopNum(int atracID, int nloops) {
 		Atrac3Object atrac3Object = hleEmulatorState.uniqueIdFactory.get!Atrac3Object(atracID);
 		atrac3Object.nloops = nloops;
+		return 0;
+	}
+	
+	/**
+	 * Gets the remaining (not decoded) number of frames
+	 * 
+	 * @param atracID - the atrac ID
+	 * @param outRemainFrame - pointer to a integer that receives either -1 if all at3 data is already on memory, 
+	 *  or the remaining (not decoded yet) frames at memory if not all at3 data is on memory 
+	 *
+	 * @return < 0 on error, otherwise 0
+	 *
+	*/
+	int sceAtracGetRemainFrame(int atracID, int *outRemainFrame) {
+		Atrac3Object atrac3Object = hleEmulatorState.uniqueIdFactory.get!Atrac3Object(atracID);
+		logWarning("Not implemented sceAtracGetRemainFrame(%d, %s)", atracID, outRemainFrame);
+		*outRemainFrame = 0;
+		return 0;
+	}
+	
+	/**
+	 * Decode a frame of data. 
+	 *
+	 * @param atracID        - the atrac ID
+	 * @param outSamples     - pointer to a buffer that receives the decoded data of the current frame
+	 * @param outN           - pointer to a integer that receives the number of audio samples of the decoded frame
+	 * @param outEnd         - pointer to a integer that receives a boolean value indicating if the decoded frame is the last one
+	 * @param outRemainFrame - pointer to a integer that receives either -1 if all at3 data is already on memory, 
+	 *                         or the remaining (not decoded yet) frames at memory if not all at3 data is on memory
+	 *
+	 * @return < 0 on error, otherwise 0
+	 */
+	int sceAtracDecodeData(int atracID, u16 *outSamples, int *outN, int *outEnd, int *outRemainFrame) {
+		logWarning("Not implemented sceAtracDecodeData(%d)", atracID);
+
+		Atrac3Object atrac3Object = hleEmulatorState.uniqueIdFactory.get!Atrac3Object(atracID);
+		*outSamples = 0;
+		*outN = 0;
+		*outEnd = 0;
+		*outRemainFrame = 0;
+		return 0;
+	}
+	
+	/**
+	 * It releases an atrac ID
+	 *
+	 * @param atracID - the atrac ID to release
+	 *
+	 * @return < 0 on error
+	 *
+	*/
+	int sceAtracReleaseAtracID(int atracID) {
+		hleEmulatorState.uniqueIdFactory.remove!Atrac3Object(atracID);
 		return 0;
 	}
 }
